7d623a84331257fc510a1758e554c0223e227e3b
SeungEunChun <1000tmddms@gmail.com>
2024-02-22 15:42:56 +0900

    환경변수 재설정

--- components/search.tsx
+++ components/search.tsx
@@ -2,16 +2,21 @@
 
 import { useState } from 'react';
 
-export default function Home() {
+export default function Search() {
     const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+    // const transKey = process.env.NEXT_PUBLIC_TRANSLATE_API_KEY
     const [city, setCity] = useState('');
     const [weather, setWeather] = useState(null);
-    const Celsius = 273;
+
+
+    // const fetchTranslate = async (e) => {
+    //     const res = await fetch()
+    // }
 
 
     const fetchWeather = async (e) => {
         e.preventDefault();
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=kr&units=metric`);
 
         const data = await response.json();
         setWeather(data);
@@ -32,8 +37,8 @@ export default function Home() {
             {weather && (
                 <div className='container'>
                     <h1> {weather.name} 날씨 정보</h1>
-                    <p>기온 : {(weather.main.temp - Celsius).toFixed(1)}°C</p>
-                    <p>체감온도 : {(weather.main.feels_like - Celsius).toFixed(1)}°C</p>
+                    <p>기온 : {(weather.main.temp).toFixed(1)}°C</p>
+                    <p>체감온도 : {(weather.main.feels_like).toFixed(1)}°C</p>
                     <p>상태 : {weather.weather[0].description}</p>
                     <p>풍속 : {(weather.wind.speed).toFixed(1)}m/s</p>
                     {/* 아이콘실험 */}
