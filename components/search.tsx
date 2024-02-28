69cfb1ed20b3295ab47ebc7c3fe3f18b3e5bd1f7
SeungEunChun <1000tmddms@gmail.com>
2024-02-29 01:09:55 +0900

    검색결과 팝업처럼 띄우기

--- components/search.tsx
+++ components/search.tsx
@@ -53,6 +53,7 @@ export default function Search() {
 
         const data = await response.json();
         setWeather(data);
+        document.body.classList.add("dim")
     };
 
     // const handleClick = () => {
@@ -96,18 +97,19 @@ export default function Search() {
 
                 </div> */}
                 {weather && (
-                    <div className={`w-100 text-center mt-5 mb-5 searchresult mx-auto bg${weather.weather[0].icon}`}>
-                        <h2> {weather.name} 날씨 정보</h2>
-                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weathericon" />
+                    <div className='w-100 active zup'>
+                        <div id='result' className={`w-100 text-center mt-5 mb-5 searchresult bg${weather.weather[0].icon}`}>
+                            <h2> {weather.name} 날씨 정보</h2>
+                            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weathericon" />
 
-                        <p>기온 : {(weather.main.temp).toFixed(1)}°C</p>
-                        <p>체감온도 : {(weather.main.feels_like).toFixed(1)}°C</p>
-                        <p>상태 : {weather.weather[0].description}</p>
-                        <p>풍속 : {(weather.wind.speed).toFixed(1)}m/s</p>
+                            <p>기온 : {(weather.main.temp).toFixed(1)}°C</p>
+                            <p>체감온도 : {(weather.main.feels_like).toFixed(1)}°C</p>
+                            <p>상태 : {weather.weather[0].description}</p>
+                            <p>풍속 : {(weather.wind.speed).toFixed(1)}m/s</p>
 
 
+                        </div>
                     </div>
-
                 )
                 }
             </div>
