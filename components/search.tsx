519533e2c01654039df2ea22002edfedd1544cfb
SeungEunChun <1000tmddms@gmail.com>
2024-03-01 04:45:29 +0900

    search 컴포넌트에 TypeScript 를 적용하여 데이터 전달과 값 처리 방식에 안정성을 더함./검색 결과로 나온 날씨 결과 div에 버튼 추가하여 닫기 활성화

--- components/search.tsx
+++ components/search.tsx
@@ -1,70 +1,41 @@
 "use client"
+import React, { useState } from 'react';
 
-import { useState } from 'react';
+interface WeatherData {
+    name: string;
+    weather: [
+        {
+            icon: string;
+            description: string;
+        }
+    ];
+    main: {
+        temp: number;
+        feels_like: number;
+    };
+    wind: {
+        speed: number;
+    };
+}
 
+export default function Search(): JSX.Element {
+    const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY as string;
 
-export default function Search() {
-    // const cityArr = {
-    //     ko: [
-    //         "서울",
-    //         "도쿄",
-    //         "런던",
-    //         "파리",
-    //         "베이징",
-    //         "아테네",
-    //         "헬싱키",
-    //         "모스크바",
-    //         "뉴욕",
-    //         "로마"
+    const [city, setCity] = useState<string>('');
+    const [weather, setWeather] = useState<WeatherData | null>(null);
 
-    //     ],
-    //     en: [
-    //         "Seoul",
-    //         "Tokyo",
-    //         "London",
-    //         "Paris",
-    //         "Beijing",
-    //         "Athens",
-    //         "Helsinki",
-    //         "Moscow",
-    //         "New York",
-    //         "Rome"
-    //     ]
-    // }
-    const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+    const handleClick = () => {
+        setWeather(null);
+    }
 
-    const [city, setCity] = useState('');
-    const [weather, setWeather] = useState(null);
-    // const [recent, setRecent] = useState(false);
-
-
-
-
-
-
-
-
-
-
-
-    const FetchWeather = async (e) => {
+    const FetchWeather = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=kr&units=metric`);
 
-        const data = await response.json();
+        const data: WeatherData = await response.json();
         setWeather(data);
-        document.body.classList.add("dim")
+        document.body.classList.add("dim");
     };
-
-    // const handleClick = () => {
-    //     setRecent((prevRecent) => !prevRecent)
-    // }
-
-
-
-
-
-
 
     return (
         <div>
@@ -78,42 +49,21 @@ export default function Search() {
                         required
                     />
                     <button className='' type="submit">검색</button>
-                </form >
+                </form>
             </div>
-            <div className='d-md-flex'>
-                {/* <div>
-                    <strong onClick={handleClick} className='examplebtn'>도시 예시</strong>
-                    {recent &&
-                        <ul className='recenttag mt-5 position-fixed'>
-                            {
-                                cityArr["ko"].map((e, i) => {
-                                    return (
-                                        <li key={`location${i}`} onClick={() => { setCity(cityArr["en"][i]) }}>{e}</li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    }
-
-                </div> */}
-                {weather && (
-                    <div className='w-100 active zup'>
-                        <div id='result' className={`w-100 text-center mt-5 mb-5 searchresult bg${weather.weather[0].icon}`}>
-                            <h2> {weather.name} 날씨 정보</h2>
-                            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weathericon" />
-
-                            <p>기온 : {(weather.main.temp).toFixed(1)}°C</p>
-                            <p>체감온도 : {(weather.main.feels_like).toFixed(1)}°C</p>
-                            <p>상태 : {weather.weather[0].description}</p>
-                            <p>풍속 : {(weather.wind.speed).toFixed(1)}m/s</p>
-
-
-                        </div>
+            {weather && (
+                <div className='w-100 active zup'>
+                    <div id='result' className={`w-100 text-center mt-5 mb-5 searchresult bg${weather.weather[0].icon}`}>
+                        <h2> {weather.name} 날씨 정보</h2>
+                        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weathericon" />
+                        <p>기온 : {(weather.main.temp).toFixed(1)}°C</p>
+                        <p>체감온도 : {(weather.main.feels_like).toFixed(1)}°C</p>
+                        <p>상태 : {weather.weather[0].description}</p>
+                        <p>풍속 : {(weather.wind.speed).toFixed(1)}m/s</p>
+                        <div onClick={handleClick}>닫기</div>
                     </div>
-                )
-                }
-            </div>
-
-        </div >
+                </div>
+            )}
+        </div>
     );
 }
