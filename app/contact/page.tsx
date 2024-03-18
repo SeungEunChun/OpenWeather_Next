6d409553f1fedf8d757fad0246b183386a01188d
SeungEunChun <1000tmddms@gmail.com>
2024-03-19 01:22:02 +0900

    contact쪽 각 아이콘에 keyframe animation을 적용하여 페이지가 마운트될때 요소가 왼쪽에서 원래 위치로 이동하도록 구현

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,12 +16,12 @@ export default function PageContact() {
         </div>
         <div>
             <ul className='contactlist d-lg-flex justify-content-between text-center'>
-                <li><a href="https://github.com/SeungEunChun" className='d-block mx-3' target='_blank'><Github /></a></li>
-                <li><a href="https://www.notion.so/OpenWeatherMap-API-3a2d3388718f41f0815b4e1c943468af" className='d-block mx-3' target='_blank'><Notion /></a></li>
-                <li><a href="https://www.instagram.com/dancer__ted?igsh=bG04cXBxdGQ2a3c2" className='d-block mx-3' target='_blank'><Instagram /></a></li>
+                <li className='icon'><a href="https://github.com/SeungEunChun" className='d-block mx-3' target='_blank'><Github /></a></li>
+                <li className='icon'><a href="https://www.notion.so/OpenWeatherMap-API-3a2d3388718f41f0815b4e1c943468af" className='d-block mx-3' target='_blank'><Notion /></a></li>
+                <li className='icon'><a href="https://www.instagram.com/dancer__ted?igsh=bG04cXBxdGQ2a3c2" className='d-block mx-3' target='_blank'><Instagram /></a></li>
 
             </ul>
-            <ul className='ms-lg-5 ms-3 contactlist'>
+            <ul className='contact ms-lg-5 ms-3 contactlist'>
                 <li>Tel : <i>010-3084-6118</i></li>
                 <li>E-Mail : <i>1000tmddms@gmail.com</i></li>
 
